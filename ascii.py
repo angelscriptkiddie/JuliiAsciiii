2f93b034ed41665c5e2be7bbfa98355e6043ccf9
LittleSwag <76930165+angelscriptkiddie@users.noreply.github.com>
2022-02-28 16:53:59 -0600

    Update ascii.py

--- ascii.py
+++ ascii.py
@@ -5,5 +5,5 @@ init(strip=not sys.stdout.isatty()) # strip colors if stdout is redirected
 from termcolor import cprint 
 from pyfiglet import figlet_format
 
-cprint(figlet_format('Julii<3!', font='starwars'),
-       'yellow', 'on_blue', attrs=['bold'])
+cprint(figlet_format('Julii<3!', font='isometric1'),
+       'yellow', 'on_pink', attrs=['bold'])
